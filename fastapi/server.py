38693a57a8801a5ea4363d80f85e0a4e52d9bffd
Ashish Trehan <ashishtrehan@Ashishs-MacBook-Pro.local>
2021-04-27 21:58:55 -0400

    testing post using FastAPI

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,18 +1,37 @@
 import io
-
 from starlette.responses import Response
-
 from fastapi import FastAPI, File
+from pydantic import BaseModel
+from typing import Any, Dict, AnyStr, List, Union
+
+
+
+class Sentence(BaseModel):
+    text: str
+
+class Contact(BaseModel):
+    contact_id:int
+    first_name:str
+    last_name:str
+    user_name:str
+    password:str
 
 
 app = FastAPI(
-    title="DeepLabV3 image segmentation",
-    description="""Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
-                           Visit this URL at port 8501 for the streamlit interface.""",
+    title="Outlier Detection",
+    description="""Pass Outlier Data and return Outliers""",
     version="0.1.0",
 )
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post('/lowercase')
+def lower_case(sentence: Sentence):
+    return {'text': sentence.text.lower()}
+
+@app.post('/contact')
+async def create_contact(contact: Contact):
+    return contact
 
 
